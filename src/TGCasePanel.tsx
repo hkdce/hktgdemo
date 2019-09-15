10c0425b2c3666c2b450ba2e4a7acf9071c11bc9
ycchan <dadfingtg@gmail.com>
2019-09-15 09:20:49 -0700

    Remove the video players' code from TGCasePanel
    Signed-off-by: ycchan <dadfingtg@gmail.com>

--- src/TGCasePanel.tsx
+++ src/TGCasePanel.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { connect } from 'react-redux';
 import { TGCase, ReduxState } from './Types';
-import { tgCases, videoRefs } from './data/Data';
-import ReactPlayer from 'react-player'
+import { tgCases } from './data/Data';
 
 type StateProps = {
   caseId?: string;
@@ -17,45 +16,11 @@ type DispatchProps = {
 
 type Props = StateProps & OwnProps & DispatchProps;
 
-function renderVideoPlayer(iVideoEvidence:string[]){
-
-    let iRefVideoCode:string = iVideoEvidence[0];
-    let elapsedSeconds:number = getElapsedSecondsFromTimeString(iVideoEvidence[1]);
-    let iCompiledURL:string = videoRefs[iRefVideoCode]['url'].concat('?t=', elapsedSeconds.toString());
-
-    let player = React.createRef<ReactPlayer>();
-
-    function handleVideoStart(){
-      if (player.current){
-        player.current.seekTo(elapsedSeconds)
-      }
-    }
-
-    return (<ReactPlayer 
-            ref={player}
-            url={iCompiledURL}
-            playing={false} 
-            controls={true}
-            onStart={handleVideoStart}
-            width={'75%'}
-            height={'75%'}
-            />)    
-
-}
-
-// Function for converting hh:mm:ss string to elapsed seconds
-function getElapsedSecondsFromTimeString(timeString: string):number{
-    // Presume the timeString is in the format of hh:mm:ss
-    let components = timeString.split(':');
-    let elapsedSeconds:number = parseInt(components[0])*3600 +parseInt(components[1])*60 + parseInt(components[2]);
-    return elapsedSeconds;
-}
 
 const TGCasePanel: React.FunctionComponent<Props> = (props) => {
   if (props.caseId) {
     const tgCase: TGCase = tgCases[props.caseId];
     return (
-      <div>
       <Card>
         <Card.Header>{ tgCase.case_id }</Card.Header>
         <Card.Body>
@@ -64,13 +29,6 @@ const TGCasePanel: React.FunctionComponent<Props> = (props) => {
           <div>Quantity: { tgCase.quantity }</div>
         </Card.Body>
       </Card>
-      <div className="videosCol">
-          {tgCase.live_video_refs.map(iVideoEvidence => {
-            return renderVideoPlayer(iVideoEvidence)
-          })}
-      </div>
-
-      </div>
     );
   } else {
     return null;
