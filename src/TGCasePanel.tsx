87c933ba992a89fd45f4b9b28bcf5451fbd59d44
ycchan <dadfingtg@gmail.com>
2019-09-15 00:13:11 -0700

    Create the functions required for our video player
    Signed-off-by: ycchan <dadfingtg@gmail.com>

--- src/TGCasePanel.tsx
+++ src/TGCasePanel.tsx
@@ -17,6 +17,40 @@ type DispatchProps = {
 
 type Props = StateProps & OwnProps & DispatchProps;
 
+function renderVideoPlayer(iVideoEvidence:string[]){
+
+    let iRefVideoCode:string = iVideoEvidence[0];
+    let elapsedSeconds:number = getElapsedSecondsFromTimeString(iVideoEvidence[1]);
+    let iCompiledURL:string = videoRefs[iRefVideoCode]['url'].concat('?t=', elapsedSeconds.toString());
+
+    let player = React.createRef<ReactPlayer>();
+
+    function handleVideoStart(){
+      if (player.current){
+        player.current.seekTo(elapsedSeconds)
+      }
+    }
+
+    return (<ReactPlayer 
+            ref={player}
+            url={iCompiledURL}
+            playing={false} 
+            controls={true}
+            onStart={handleVideoStart}
+            width={'75%'}
+            height={'75%'}
+            />)    
+
+}
+
+// Function for converting hh:mm:ss string to elapsed seconds
+function getElapsedSecondsFromTimeString(timeString: string):number{
+    // Presume the timeString is in the format of hh:mm:ss
+    let components = timeString.split(':');
+    let elapsedSeconds:number = parseInt(components[0])*3600 +parseInt(components[1])*60 + parseInt(components[2]);
+    return elapsedSeconds;
+}
+
 const TGCasePanel: React.FunctionComponent<Props> = (props) => {
   if (props.caseId) {
     const tgCase: TGCase = tgCases[props.caseId];
