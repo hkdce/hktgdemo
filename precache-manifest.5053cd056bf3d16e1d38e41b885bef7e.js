1de6659ceb7fe224174248ca2994e24bf909f205
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-15 16:25:24 +0000

    Deploy hkdce/hktgdemo to github.com/hkdce/hktgdemo.git:gh-pages

---
+++ precache-manifest.5053cd056bf3d16e1d38e41b885bef7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726eb26c90334bcbbd77375c9643ccc9",
+    "url": "/hktgdemo/index.html"
+  },
+  {
+    "revision": "9ff880a28177b79ba8ec",
+    "url": "/hktgdemo/static/css/2.adca5a3a.chunk.css"
+  },
+  {
+    "revision": "6c684fc89670d939bfa1",
+    "url": "/hktgdemo/static/css/main.ea54f418.chunk.css"
+  },
+  {
+    "revision": "9ff880a28177b79ba8ec",
+    "url": "/hktgdemo/static/js/2.5e14dbaf.chunk.js"
+  },
+  {
+    "revision": "6c684fc89670d939bfa1",
+    "url": "/hktgdemo/static/js/main.8d09adfb.chunk.js"
+  },
+  {
+    "revision": "b736b234a5e378255f69",
+    "url": "/hktgdemo/static/js/runtime~main.3059f623.js"
+  }
+]);
